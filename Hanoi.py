51aa10486aa2cbe993c9131fb1929e732191a2bf
SamNzo <sam.nzongani@gmail.com>
2023-01-09 16:10:34 +0100

    start config

--- Hanoi.py
+++ Hanoi.py
@@ -1,6 +1,17 @@
 class HanoiConfiguration:
-    def __init__(self):
-        pass
+    def __init__(self, nbTower: int, nbDisk: int):
+        self.nbTower = nbTower
+        self.nbDisk = nbDisk
+        self.towers = []
 
-    def __str__(self):
-        pass
+        for i in range(self.nbTower):
+            self.towers.append([])
+
+        for i in range(self.nbDisk):
+            self.towers[0].append(self.nbDisk - i)
+
+        print(self.towers)
+
+
+if __name__ == '__main__':
+    config = HanoiConfiguration(3, 4)
