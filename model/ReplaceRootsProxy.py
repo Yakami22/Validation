bd232cc4b268eed270ad2e48d7f6e1b2b563895f
Yakami22 <yassine2000.yk@gmail.com>
2023-01-13 10:19:01 +0100

    Added ReplaceRootsProxy.py

---
+++ model/ReplaceRootsProxy.py
@@ -0,0 +1,10 @@
+from model.IdentityProxy import IdentityProxy
+
+
+class ReplaceRootsProxy(IdentityProxy) :
+    def __init__(self, operand, newRoots):
+        super.__init__(operand)
+        self.newRoots = newRoots
+
+    def roots(self):
+        return self.newRoots
