f56c8b3c8a976fd984302159b3d0404711ee463a
Yakami22 <yassine2000.yk@gmail.com>
2023-01-13 11:47:29 +0100

    Completed ParentTraces

--- model/Trace.py
+++ model/Trace.py
@@ -1,8 +1,8 @@
 from model.IdentityProxy import IdentityProxy
 
 
-class ParentTraceProxy(IdentityProxy) :
-    def __init__(self,operand,dict):
+class ParentTraceProxy(IdentityProxy):
+    def __init__(self, operand, dict):
         super.__init__(operand)
         self.dict = dict
 
@@ -13,19 +13,17 @@ class ParentTraceProxy(IdentityProxy) :
         '''
         neighbours = self.operand.roots()
         for n in neighbours:
-            self.dict[n]=n
+            self.dict[n] = n
         return neighbours
 
-        ...
-
-    def next(self,source):
+    def next(self, source):
         '''
-        Not sure if this works but should return the next in graph
+        Should return the next in graph
         :param source:
         :return: next graph
         '''
         neighbours = self.operand.next(source)
         for n in neighbours:
-            if n not in self.parents:
-                self.parents[n] = source
+            if n not in self.dict:
+                self.dict[n] = source
         return neighbours
