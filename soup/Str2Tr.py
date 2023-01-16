095bad61acf787aaf562430bfe2c13bfcf2f67df
Souhaila-nouinou <souhaila.nouinou22@gmail.com>
2023-01-16 16:50:17 +0100

    STR2TR update

--- soup/Str2Tr.py
+++ soup/Str2Tr.py
@@ -1,9 +1,18 @@
 from model.TransitionRelation import TransitionRelation
+from soup.SemanticTransitionRelation import SemanticTransitionRelation
 
 class Str2Tr(TransitionRelation):
+
+    def __init__(self, operand) :
+        pass
+        
     def getRoots(self):
-        return super().getRoots()
-        # str.initalConfiguration()
+        # return self.getRoots()
+        return SemanticTransitionRelation.initialConfiguration()
 
     def next(self, source):
-        return super().next(source)
+        A = SemanticTransitionRelation.enabledActions(source)
+        r= []
+        for a in A:
+            r.append(SemanticTransitionRelation.execute(a,source))
+        return r 
