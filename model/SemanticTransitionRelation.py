4f733c659cec6afb8d14ae13ed2d3648b2555130
Souhaila-nouinou <souhaila.nouinou22@gmail.com>
2023-01-16 14:13:33 +0100

    SemanticTransitionRelation class and methods

---
+++ model/SemanticTransitionRelation.py
@@ -0,0 +1,12 @@
+class SemanticTransitionRelation:
+    def initialConfiguration():
+        #returns a list C
+        pass 
+
+    def enabledActions(source):
+        #return List<A>
+        pass
+
+    def execute( action, source):
+        #return List C
+        pass
