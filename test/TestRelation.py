38fe7f7e4c56f924e286299eb4cbbd2f676cedd3
Sam <sam.nzongani@gmail.com>
2023-01-16 15:44:18 +0100

    added unit tests for dictGraph/nBits/hanoi

--- test/TestRelation.py
+++ test/TestRelation.py
@@ -9,20 +9,26 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.dictGraph = DictGraph({1: [2, 3], 2: [5, 6], 3: [], 4: [4, 6], 5: [4], 6: [6]}, [])
         self.nBits = NBits([0, 1, 2], 3)
-        #self.hanoiConfiguration = HanoiConfiguration(3, 4)
-        #self.hanoi = HanoiRules([[3, 1], [2], []])
+        self.hanoiConfiguration = HanoiConfiguration(3, 4)
+        self.hanoi = HanoiRules([[3, 1], [2], []])
 
     def test_dictGraph(self):
         self.assertEqual(self.dictGraph.next(1), [2, 3])
         self.assertEqual(self.dictGraph.next(3), [])
 
-'''
     def test_nBits(self):
-        #self.assertEqual(self.nBits.next(3), )
+        self.assertIn(1, self.nBits.next(3))
+        self.assertIn(2, self.nBits.next(3))
+        self.assertIn(7, self.nBits.next(3))
+        self.assertNotIn(3, self.nBits.next(3))
 
     def test_hanoi(self):
-        pass
-'''
+        self.assertIn([[3, 1], [2], []], self.hanoi.next([[3, 1], [2], []]))
+        self.assertIn([[3], [2, 1], []], self.hanoi.next([[3, 1], [2], []]))
+        self.assertIn([[3], [2], [1]], self.hanoi.next([[3, 1], [2], []]))
+        self.assertIn([[3, 1], [], [2]], self.hanoi.next([[3, 1], [2], []]))
+        self.assertNotIn([[3, 2, 1], [], []], self.hanoi.next([[3, 1], [2], []]))
+
 
 if __name__ == '__main__':
     unittest.main()
