566bf39c1ebf93d0ad2b9fd687c9de4029a08dbb
Yakami22 <yassine2000.yk@gmail.com>
2023-01-23 14:45:47 +0100

    Resloved bug with getTrace

--- model/Trace.py
+++ model/Trace.py
@@ -1,28 +1,29 @@
 from model.IdentityProxy import IdentityProxy
+from model.TransitionRelation import TransitionRelation
 
 
 class ParentTraceProxy(IdentityProxy):
-    def __init__(self, operand, dico):
-        super().__init__(operand)
-        self.dict = dico
+    def __init__(self, operand : TransitionRelation, dict):
+        super.__init__(operand)
+        self.dict = dict
 
     def roots(self):
-        """
+        '''
         Should return the roots of
         :return: graph roots
         parent racines sont eux meme
-        """
+        '''
         neighbours = self.operand.roots()
         for n in neighbours:
             self.dict[n] = n
         return neighbours
 
     def next(self, source):
-        """
+        '''
         Should return the next in graph
         :param source:
         :return: next graph
-        """
+        '''
         neighbours = self.operand.next(source)
         for n in neighbours:
             if n not in self.dict:
