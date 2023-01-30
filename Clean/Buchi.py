8bf193bcd68990c04a078a1cda2504293a1cb472
Yakami22 <yassine2000.yk@gmail.com>
2023-01-30 16:01:49 +0100

    Updated AliceAndBob.py

---
+++ Clean/Buchi.py
@@ -0,0 +1,22 @@
+from Clean.Models import SemanticTransitionRelations
+
+
+class BuchiSemantics(SemanticTransitionRelations):
+    def __init__(self, d):
+        super(BuchiSemantics).__init__()
+        self.delta = d
+
+    def initial(self):
+        return self.initial()
+
+    def actions(self,i,conf):
+        actives = []
+        actions = self.delta[conf]
+        for a in actions:
+            if a[0](i):
+                actives.append(a)
+        return actives
+
+
+def execute(self, i, conf, a):
+    return a[i]
